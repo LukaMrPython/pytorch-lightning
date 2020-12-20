c71204cedd5220a39e444bc0a76065fb77c31664
Jirka Borovec <jirka.borovec@seznam.cz>
2020-12-20 08:29:14 +0100

    mutils

--- pytorch_lightning/trainer/configuration_validator.py
+++ pytorch_lightning/trainer/configuration_validator.py
@@ -15,7 +15,7 @@ from pytorch_lightning import _logger as log
 from pytorch_lightning.core.lightning import LightningModule
 from pytorch_lightning.utilities import rank_zero_warn
 from pytorch_lightning.utilities.exceptions import MisconfigurationException
-from pytorch_lightning.utilities.model_utils import is_overridden
+from pytorch_lightning.utilities.model_helpers import is_overridden
 
 
 class ConfigValidator(object):
